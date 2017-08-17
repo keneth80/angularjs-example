274e2d292cc8a4dcb1905679433d5e051f352e7f
Insu Kim <insukim@Insuui-MacBook-Pro.local>
2017-08-17 17:17:58 +0900

    Modify TextAreaComponent and WordComponent

--- src/app/text-area-html-string-example/component/textarea.component.ts
+++ src/app/text-area-html-string-example/component/textarea.component.ts
@@ -1,17 +1,20 @@
-import { Component, Output, EventEmitter, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { Component, ViewChild, Output, EventEmitter, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-text-area-component',
     template: `
                 <div>
-                    <div class="editor" 
+                    <div #filed class="editor" 
                         contenteditable="true" 
-                        [innerHTML]="htmlstring"
                         (click)="onClick($event);"
                         (focus)="onFocus($event);"
                         (blur)="onblur($event);"
                         (keyup)="onKeyup($event);"></div>
-                    <app-word-component >select box</app-word-component>
+                        
+                        <app-word-component 
+                            [items]="_items" 
+                            [boxPosition]="boxPosition" 
+                            [isFocusing]="isFocusing"></app-word-component>
                 </div>
               `,
     styleUrls: ['../text-area-html-string-example.css'],
@@ -19,28 +22,36 @@ import { Component, Output, EventEmitter, ChangeDetectionStrategy, ViewEncapsula
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TextAreaComponent {
+    @ViewChild('filed') filed;
 
     @Output() keyChange = new EventEmitter();
     @Output() keyComplete = new EventEmitter();
 
     htmlstring: string;
-    columnTagCss = 'column';
-    spacer = '&nbsp;&nbsp;';
-    isFocusing = false;
-    isSelectbox = '';
-    boxX = 0;
-    boxY = 0;
+    columnTagCss: string = 'column';
+    spacer: string = '&nbsp;&nbsp;';
+    isFocusing: boolean = false;
+    isSelectbox: string = '';
+    boxPosition = {
+        boxX: 0,
+        boxY: 0
+    };
 
-    private _items: Array<any>;
+    _items: Array<any> = [
+        { id: '1', label: 'label1' },
+        { id: '2', label: 'label2' },
+        { id: '3', label: 'label3' },
+        { id: '4', label: 'label4' },
+    ];
 
     constructor() {
         // test code
         this.htmlstring = `<span class="dml">SELECT</span>${this.spacer}<span class="column">column1,${this.spacer}column2</span>`
-                        + `</br>`
-                        + `<span class="dml">FROM</span>${this.spacer}<span class="column">table1</span>`
-                        + `</br>`
-                        // tslint:disable-next-line:max-line-length
-                        + `<span class="where">WHERE</span>${this.spacer}<span class="column">column1</span>${this.spacer}=${this.spacer}123`;
+            + `</br>`
+            + `<span class="dml">FROM</span>${this.spacer}<span class="column">table1</span>`
+            + `</br>`
+            // tslint:disable-next-line:max-line-length
+            + `<span class="where">WHERE</span>${this.spacer}<span class="column">column1</span>${this.spacer}=${this.spacer}123`;
     }
 
     setDMLWord(text: string): string {
@@ -48,13 +59,21 @@ export class TextAreaComponent {
     }
 
     onClick(event: any) {
+        if (!this.filed.nativeElement.innerHTML) {
+            this.isFocusing = true;
+        }
+        console.log('#filed', this.filed);
+        // this.isFocusing = true;
         console.log('onClick : ', event.clientX, event.clientY);
-        this.boxX = event.clientX;
-        this.boxY = event.clientY;
+
+        this.boxPosition = {
+            boxX: event.clientX,
+            boxY: event.clientY
+        };
     }
 
     onFocus(event: any) {
-        this.isFocusing = true;
+        // this.isFocusing = true;
         const left: number = event.view.screenLeft;
         const top: number = event.view.screenTop;
         console.log('onFocus : ', left, top);
@@ -66,6 +85,18 @@ export class TextAreaComponent {
     }
 
     onKeyup(event: any) {
+        if (this.filed.nativeElement.innerHTML) {
+            this.isFocusing = false;
+        } else {
+            this.isFocusing = true;
+        }
+
+        if(event.keyCode === 32) { // space
+            this.isFocusing = true;
+        } else {
+            this.isFocusing = false;
+        }
+
         console.log('onKeyup : ', event);
     }
 }
--- src/app/text-area-html-string-example/component/word.component.ts
+++ src/app/text-area-html-string-example/component/word.component.ts
@@ -1,16 +1,35 @@
-import { Component, Input, Output, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-word-component',
     template: `
-                <div class="word" 
+                <ul class="word-list" 
+                 *ngIf="isFocusing"
+                     (keyup)="keyUp($event)"
                      contenteditable="true" 
-                     [innerHTML]="htmlstring"></div>
+                     [ngStyle]="setStyles()">
+
+                    <li *ngFor="let item of items">
+                        <a class="dropdown-item">
+                            <span>{{item.label}}</span>
+                        </a>
+                    </li>
+                </ul>
               `,
     styleUrls: ['../text-area-html-string-example.css'],
     encapsulation: ViewEncapsulation.Native
 })
-export class WordComponent {
+export class WordComponent implements OnChanges {
+    @Input() boxPosition: any;
+    @Input() isFocusing: boolean;
+    @Input()
+    set items(value: Array<any>) {
+        this._items = value;
+    }
+
+    get items(): Array<any> {
+        return this._items;
+    }
 
     @Input()
     set word(value: string) {
@@ -23,16 +42,35 @@ export class WordComponent {
 
     htmlstring: string;
     columnTagCss = 'column';
-    isFocusing = false;
 
     private _word: string;
+    private _items: Array<any>;
 
     constructor() {
         this.htmlstring = 'This select box!';
+    }
+
+    ngOnChanges() {
+        console.log('ngOnChanges', this.isFocusing);
     }
 
     setDMLWord(text: string): string {
         return `<span class="dml">${text}</span>`;
     }
 
+    setStyles() {
+        console.log(this.boxPosition);
+        let styles = {
+            // CSS property names
+            'left': this.boxPosition.boxX + 'px',
+            'top': this.boxPosition.boxY + 'px',
+            'position': 'absolute'
+        };
+
+        return styles;
+    }
+
+    keyUp(event) {
+        console.log('keyUp', event);
+    }
 }
